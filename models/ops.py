yes.. it was wrong. fixed perplexity calculation

models/ops.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from tensorflow.models.rnn import rnn_cell
 
-
 def conv2d(inputs, out_dim, k_w, k_h, stddev=0.02, name="conv2d", bias_start=0):
   if not isinstance(inputs, (list, tuple)):
     inputs = [inputs]
@@ -10,7 +9,7 @@ def conv2d(inputs, out_dim, k_w, k_h, stddev=0.02, name="conv2d", bias_start=0):
     input_ = tf.concat(3, inputs)
 
     w = tf.get_variable('conv_k', [k_w, k_h, input_.get_shape()[-1], out_dim],
-              initializer=tf.truncated_normal_initializer(stddev=stddev))
+                        initializer=tf.contrib.layers.xavier_initializer_conv2d())
 
     if len(inputs) == 1:
       conv = tf.nn.conv2d(inputs[0], w, [1, 1, 1, 1], "SAME")
@@ -23,3 +22,50 @@ def conv2d(inputs, out_dim, k_w, k_h, stddev=0.02, name="conv2d", bias_start=0):
     bias = tf.get_variable("conv_b", [out_dim],
                            initializer=tf.constant_initializer(0.0))
     return conv + bias + bias_start
+
+def linear(args, output_size, bias, bias_start=0.0, scope=None):
+  """Code modification of tensorflw/python/ops/rnn_cell.py
+  Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
+
+  Args:
+    args: a 2D Tensor or a list of 2D, batch x n, Tensors.
+    output_size: int, second dimension of W[i].
+    bias: boolean, whether to add a bias term or not.
+    bias_start: starting value to initialize the bias; 0 by default.
+    scope: VariableScope for the created subgraph; defaults to "Linear".
+  Returns:
+    A 2D Tensor with shape [batch x output_size] equal to
+    sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
+  Raises:
+    ValueError: if some of the arguments has unspecified or wrong shape.
+  """
+  if args is None or (isinstance(args, (list, tuple)) and not args):
+    raise ValueError("`args` must be specified")
+  if not isinstance(args, (list, tuple)):
+    args = [args]
+
+  # Calculate the total size of arguments on dimension 1.
+  total_arg_size = 0
+  shapes = [a.get_shape().as_list() for a in args]
+  for shape in shapes:
+    if len(shape) != 2:
+      raise ValueError("Linear is expecting 2D arguments: %s" % str(shapes))
+    if not shape[1]:
+      raise ValueError("Linear expects shape[1] of arguments: %s" % str(shapes))
+    else:
+      total_arg_size += shape[1]
+
+  # Now the computation.
+  with tf.variable_scope(scope or "Linear"):
+    matrix = tf.get_variable("Matrix", [total_arg_size, output_size],
+                             initializer=tf.contrib.layers.xavier_initializer())
+    if len(args) == 1:
+      res = tf.matmul(args[0], matrix)
+    else:
+      res = tf.matmul(array_ops.concat(1, args), matrix)
+    if not bias:
+      return res
+    bias_term = tf.get_variable(
+        "Bias", [output_size],
+        initializer=tf.constant_initializer(bias_start))
+  return res + bias_term

models/rcmn.py
@@ -63,7 +63,8 @@ class RCMN(BaseModel):
       self.x = tf.placeholder(tf.int32, [self.batch_size, self.max_seq_l])
 
       with tf.device("/cpu:0"):
-        embedding = tf.get_variable("embedding", [self.vocab_size, self.embed_dim])
+        embedding = tf.get_variable("embedding", [self.vocab_size, self.embed_dim],
+                                    initializer=tf.contrib.layers.xavier_initializer())
         first_input = tf.nn.embedding_lookup(embedding, self.x)
 
       if self.is_training and self.keep_prob < 1:
@@ -139,7 +140,7 @@ class RCMN(BaseModel):
         self.Y_ = []
         for step, output in enumerate(outputs):
           # [self.batch_size, self.vocab_size]
-          logits = rnn_cell.linear(output, self.vocab_size, True, scope="y_S%d" % step)
+          logits = linear(output, self.vocab_size, True, scope="y_S%d" % step)
           self.Y_.append(tf.reshape(logits, [self.batch_size, -1]))
       else:
         self.y = tf.placeholder(tf.int32, [self.batch_size, self.max_seq_l], name="y")
@@ -150,7 +151,7 @@ class RCMN(BaseModel):
         self.Y_ = []
         for step, output in enumerate(outputs):
           # [self.batch_size x self.max_seq_l, self.vocab_size]
-          logits = rnn_cell.linear(output, self.max_seq_l * self.vocab_size, True, scope="y_S%d" % step)
+          logits = linear(output, self.max_seq_l * self.vocab_size, True, scope="y_S%d" % step)
           self.Y_.append(tf.reshape(logits, [self.batch_size * self.max_seq_l, -1]))
 
     with tf.variable_scope("training"):
@@ -246,24 +247,26 @@ class RCMN(BaseModel):
       else:
         data = {self.x: x, self.y: y, self.initial_state: state}
 
-      if step % 100 == 1:
+      if step % 100 == 99:
         cost, state = self.sess.run([self.cost, self.final_state], data)
         print(" [*] %.3f perplexity: %.3f speed: %.0f wps" %
               (step * 1.0 / epoch_size, np.exp(costs / iters),
               iters * self.batch_size / (time.time() - start_time)))
 
-      if step % 5 == 1:
+      if step % 5 == 4:
         cost, state, summary_str, _ = self.sess.run(
             [self.cost, self.final_state, summary, self.optim], data)
-
         writer.add_summary(summary_str, self.g_step.eval())
       else:
         cost, state, _ = self.sess.run([self.cost, self.final_state, self.optim], data)
 
       costs += cost
-      iters += self.num_steps
+      if self.is_single_output:
+        iters += 1
+      else:
+        iters += self.num_steps
 
-    pred_words = [self.idx2word[i] for i in self.sess.run([self.pred], data)]
+    pred_words = [self.idx2word[i] for i in self.sess.run(self.pred, data)]
     print("\n\n".join([" ".join([self.idx2word[i] for i in l]) \
                         + ' "%s"' % w for l, w in zip(x, pred_words)]))
 